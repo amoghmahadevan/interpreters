fe1bf3768e0d8e14a6e83587fc0ca9561b45f56a
amoghmahadevan <amogh.mahadevan1@gmail.com>
2025-06-11 17:10:54 -0700

    functions

--- JavaInterpreter/jlox/Interpreter.java
+++ JavaInterpreter/jlox/Interpreter.java
@@ -1,10 +1,30 @@
 package jlox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+  final Environment globals = new Environment();
+  private Environment environment = globals;
 
-  private Environment environment = new Environment();
+  Interpreter() {
+    globals.define("clock", new LoxCallable() {
+      @Override
+      public int arity() {
+        return 0;
+      }
+
+      @Override
+      public Object call(Interpreter interpreter, List<Object> arguments) {
+        return (double) System.currentTimeMillis() / 1000.0;
+      }
+
+      @Override
+      public String toString() {
+        return "<native fn>";
+      }
+    });
+  }
 
   void interpret(List<Stmt> statements) {
     try {
@@ -104,6 +124,29 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   }
 
   @Override
+  public Object visitCallExpr(Expr.Call expr) {
+    Object callee = evaluate(expr.callee);
+    List<Object> arguments = new ArrayList<>();
+    for (Expr argument : expr.arguments) {
+      arguments.add(evaluate(argument));
+    }
+
+    if (!(callee instanceof LoxCallable)) {
+      throw new RuntimeError(expr.paren,
+          "Can only call functions and classes.");
+    }
+    LoxCallable function = (LoxCallable) callee;
+
+    if (arguments.size() != function.arity()) {
+      throw new RuntimeError(expr.paren, "Expected " +
+          function.arity() + " arguments but got " +
+          arguments.size() + ".");
+    }
+
+    return function.call(this, arguments);
+  }
+
+  @Override
   public Object visitVariableExpr(Expr.Variable expr) {
     return environment.get(expr.name);
   }
@@ -123,7 +166,9 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     statement.accept(this);
   }
 
-  private void executeBlock(List<Stmt> statements, Environment environment) {
+  // When executing block, we give it block we want it to execute and environment
+  // it needs to hop into
+  void executeBlock(List<Stmt> statements, Environment environment) {
     Environment previous = this.environment;
     try {
       this.environment = environment;
@@ -148,6 +193,13 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   }
 
   @Override
+  public Void visitFunctionStmt(Stmt.Function stmt) {
+    LoxFunction function = new LoxFunction(stmt, environment);
+    environment.define(stmt.name.lexeme, function);
+    return null;
+  }
+
+  @Override
   public Void visitIfStmt(Stmt.If stmt) {
     if (isTruthy(evaluate(stmt.condition))) {
       execute(stmt.thenBranch);
@@ -162,6 +214,14 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     Object value = evaluate(stmt.expression);
     System.out.println(stringify(value));
     return null;
+  }
+
+  @Override
+  public Void visitReturnStmt(Stmt.Return stmt) {
+    Object value = null;
+    if (stmt.value != null)
+      value = evaluate(stmt.value);
+    throw new Return(value);
   }
 
   @Override
---
+++ JavaInterpreter/jlox/LoxFunction.java
@@ -0,0 +1,40 @@
+package jlox;
+
+import java.util.List;
+
+public class LoxFunction implements LoxCallable {
+  private final Stmt.Function declaration;
+  private final Environment closure;
+
+  LoxFunction(Stmt.Function declaration, Environment closure) {
+    this.closure = closure;
+    this.declaration = declaration;
+  }
+
+  // Most important line of code!!
+  @Override
+  public Object call(Interpreter interpreter, List<Object> arguments) {
+    // We create a new environment at each CALL, NOT at function declaration!
+    // Closure is the environment that is active when function is DECLARED
+    Environment environment = new Environment(closure);
+    for (int i = 0; i < declaration.params.size(); i++) {
+      environment.define(declaration.params.get(i).lexeme, arguments.get(i));
+    }
+    try {
+      interpreter.executeBlock(declaration.body, environment);
+    } catch (Return returnValue) {
+      return returnValue.value;
+    }
+    return null;
+  }
+
+  @Override
+  public int arity() {
+    return declaration.params.size();
+  }
+
+  @Override
+  public String toString() {
+    return "<fn " + declaration.name.lexeme + ">";
+  }
+}
